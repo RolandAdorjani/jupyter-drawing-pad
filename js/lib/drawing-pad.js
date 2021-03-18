194b0bd67ed89d6f34f6693e48c1b1d47a1ef1da
RolandAdorjani <adorjanir@yahoo.com>
2021-03-18 22:22:10 +0000

    Update drawing-pad.js

--- js/lib/drawing-pad.js
+++ js/lib/drawing-pad.js
@@ -8,8 +8,8 @@ var create = function (that) {
 
 	var sketch = that.sketch;
 	var sketch_style = getComputedStyle(sketch);
-	canvas.width = 700;
-	canvas.height = 1000;
+	canvas.width = 500;
+	canvas.height = 500;
 
 	var mouse = {x: 0, y: 0, t:0};
 	window.that = that;
